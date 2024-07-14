f8b6e251cf99f7ef11c54a440a14fa6cf1f5d47a
amsmoox <amsmoox@gmail.com>
2024-07-14 12:37:12 +0100

    add View/urls/serializers/admin

--- BizNass/services/views.py
+++ BizNass/services/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import ServiceCategory, ServiceProvider, ServiceReview
+from .serializers import ServiceCategorySerializer, ServiceProviderSerializer, ServiceReviewSerializer
 
 # Create your views here.
+
+class ServiceCategoryViewset(viewsets.ModelViewSet):
+    queryset = ServiceCategory.object.all()
+    serializer_class = ServiceCategorySerializer
+
+class ServiceProviderViewset(viewsets.ModelViewSet):
+    queryset = ServiceProvider.object.all()
+    serializer_class = ServiceProviderSerializer
+
+class ServiceReviewViewset(viewsets.ModelViewSet):
+    queryset = ServiceReview.object.all()
+    serializer_class = ServiceReviewSerializer
