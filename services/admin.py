e5cdc9c1880a753962fc3c3749009ab623be4225
MHARRECH Ayoub <82274806+Amsmoox@users.noreply.github.com>
2024-08-11 10:58:21 +0100

    Delete services directory

--- services/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import ServiceCategory, ServiceProvider, ServiceReview
-# Register your models here.
-
-
-admin.site.register(ServiceCategory)
-admin.site.register(ServiceProvider)
-admin.site.register(ServiceReview)
